0fdee365877fe4b114133bfcc2097637baab283a
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2012-09-13 00:19:01 +0200

    rect or square

--- pyx/placeholder.py
+++ pyx/placeholder.py
@@ -4,10 +4,11 @@ from pyx import *
 
 c = canvas.canvas()
 
-c.stroke(path.line(0, 0, 6, 0))
-c.stroke(path.line(6, 0, 6, 4))
-c.stroke(path.line(6, 4, 0, 4))
-c.stroke(path.line(0, 4, 0, 0))
+xDim, yDim = 8, 6
+c.stroke(path.line(0, 0, xDim, 0))
+c.stroke(path.line(xDim, 0, xDim, yDim))
+c.stroke(path.line(xDim, yDim, 0, yDim))
+c.stroke(path.line(0, yDim, 0, 0))
 
 
 c.writeEPSfile("placeholder")
