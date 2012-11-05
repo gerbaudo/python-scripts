813aa2cd5018ea110783dd35f54ac880ce334207
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2012-11-05 12:29:25 +0100

    add placeholder watermark

--- pyx/placeholder.py
+++ pyx/placeholder.py
@@ -9,6 +9,7 @@ c.stroke(path.line(0, 0, xDim, 0))
 c.stroke(path.line(xDim, 0, xDim, yDim))
 c.stroke(path.line(xDim, yDim, 0, yDim))
 c.stroke(path.line(0, yDim, 0, 0))
-
+c.text(0.275*xDim, 0.275*yDim, "Placeholder", [text.halign.center,text.valign.middle,trafo.rotate(40), text.size.Huge])
+c.text(0.575*xDim, 0.575*yDim, "(%dx%d)"%(xDim,yDim), [text.halign.center,text.valign.middle,trafo.rotate(40), text.size.Large])
 
 c.writeEPSfile("placeholder")
